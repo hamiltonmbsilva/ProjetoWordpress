3d904b23e36378907a67fde4a582348988f1d3d7
Hamilton <hamiltonmb.silva@gmail.com>
2019-01-25 20:03:17 -0200

    Criação do tema Wordpress
    Criação do tema do Wordpress
    Criação index, header, footer,style.css, screenshot do tema.

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso Wordepress</title>
+</head>
+<body <?php body_class(); ?>>
+<p>Este é um parágrafo de referencia</p>
+<header>
+    <section class="top-bar">
+        <div class="social-media-icons">Icones Sociais</div>
+        <div class="search"></div>
+    </section>
+    <section class="menu-area">
+        <section class="logo"></section>
+        <nav class="menu"></nav>
+    </section>
+</header>
